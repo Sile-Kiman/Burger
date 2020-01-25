7ff0fb7f16a49d3e6aca2e0273877a7a784a6929
Mamessile Kiman <sile.kiman@yahoo.com>
2020-01-25 16:15:15 -0500

    added the db to heroku

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,21 @@ var connection = mysql.createConnection({
   database: "Burgers_db"
 });
 
+var mysql = require("mysql");
+
+if(process.env.JAWSDB_URL){
+
+}else{
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "Burgers_db"
+  });
+}
+
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
