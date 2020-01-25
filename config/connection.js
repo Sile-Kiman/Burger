ab27f48c0042e2a04f589c5dfa2ae7ce4f6c38ce
Mamessile Kiman <sile.kiman@yahoo.com>
2020-01-25 16:16:37 -0500

    updated db

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "Burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "Burgers_db"
+// });
 
 var mysql = require("mysql");
 
