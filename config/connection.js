299428deab930d09a407c4ca1007ecffd6c83b8f
Mamessile Kiman <sile.kiman@yahoo.com>
2020-02-08 13:16:56 -0500

    update the connection file

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,11 @@
 var mysql = require("mysql");
+connection;
+
 
 if(process.env.JAWSDB_URL){
-
+connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
-  var connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
